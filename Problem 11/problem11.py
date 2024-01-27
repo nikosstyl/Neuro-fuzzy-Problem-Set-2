0f1794baf36c1c463d1d0ef0f03747df18303c38
Gianni-Alexandra <algianni@uth.gr>
2024-01-28 00:24:29 +0200

    finished problem11

---
+++ Problem 11/problem11.py
@@ -0,0 +1,42 @@
+import numpy as np
+from scipy.signal import convolve2d
+from skimage.measure import block_reduce
+import matplotlib.pyplot as plt
+
+# Define the pooling operation
+def max_pooling(input_matrix, pool_size, stride):
+    output_matrix = block_reduce(input_matrix, block_size=pool_size, func=np.max)
+    return output_matrix
+
+#### QUESTION A
+# Define the image matrix
+I = np.array([
+    [20, 35, 35, 35, 35, 20],
+    [29, 46, 44, 42, 42, 27],
+    [16, 25, 21, 19, 19, 12],
+    [66, 120, 116, 154, 114, 62],
+    [74, 216, 174, 252, 172, 112],
+    [70, 210, 170, 250, 170, 110]
+])
+
+# Define the stride and kernel
+stride = (1,1)
+
+kernel = np.array([
+    [1, 1, 1],
+    [1, 0, 1],
+    [1, 1, 1]
+])
+
+# Perform 2D convolution
+result = convolve2d(I, kernel, mode='valid')
+print("The result of the convolution is:\n", result)
+
+
+### QUESTION B
+# Apply max pooling
+pool_size = (2, 2)
+stride = (2, 2)  # Note: block_reduce doesn't support strides. It will always be the same as pool_size
+
+pooled_result = max_pooling(result, pool_size, stride)
+print("\nThe max pooling result is:\n", pooled_result)
---
+++ Problem 11/test.py
@@ -0,0 +1,54 @@
+import numpy as np
+from scipy.signal import convolve2d
+import matplotlib.pyplot as plt
+
+# Define the image matrix
+I = np.array([
+    [20, 35, 35, 35, 35, 20],
+    [29, 46, 44, 42, 42, 27],
+    [16, 25, 21, 19, 19, 12],
+    [66, 120, 116, 154, 114, 62],
+    [74, 216, 174, 252, 172, 112],
+    [70, 210, 170, 250, 170, 110]
+])
+
+### KERNEL F1
+F = np.array([
+     [-10, -10, -10],
+     [5, 5, 5],
+     [-10, -10, -10]
+])
+ 
+### KERNEL F2
+#F = np.array([
+#    [2, 2, -2],
+#    [2, -12, 2],
+#    [2, 2, 2]
+#])
+
+### KERNEL F3
+#F = np.array([
+#    [-20, -10, 0, 5, 10],
+#    [-10, 0, 5, 10, 5],
+#    [0, 5, 10, 5, 0],
+#    [5, 10, 5, 0, -10],
+#    [10, 5, 0, -10, -20]
+#])
+
+# Apply the kernel to the image using 2D convolution
+feature_map_F = convolve2d(I, F, mode='same', boundary='fill', fillvalue=0)
+
+# Plot the original image
+plt.subplot(1, 2, 1)
+plt.imshow(I, cmap='gray')
+plt.title('Original Image')
+plt.axis('off')
+
+# Plot the feature map
+plt.subplot(1, 2, 2)
+plt.imshow(feature_map_F, cmap='gray')
+plt.title('Feature Map')
+plt.axis('off')
+
+# Display the plots
+plt.show()
