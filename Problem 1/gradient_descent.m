1dfec5eac59709fea284256c02cdbc035aa44c62
Gianni-Alexandra <algianni@uth.gr>
2024-02-11 18:25:25 +0200

    to1

--- Problem 1/gradient_descent.m
+++ Problem 1/gradient_descent.m
@@ -20,25 +20,29 @@ tol = 1e-6;
 for i = 1:10
     %  Compute gradient
     grad = double(subs(grad_f, [x1, x2], [x(1), x(2)]));
-    fprintf("Gradient on iteration %d is: %s\n", round(i), grad);
+    % fprintf("Gradient on iteration %d is: %s\n", round(i), grad);
+    fprintf("Gradient on iteration %d is: x = %f, y = %f\n", round(i), grad(1), grad(2));
 
     %  Compute step
     step = alpha*grad;
+    %fprintf("STEP SIZE: %s", step)
+    fprintf("STEP SIZE: x = %f, y = %f\n", step(1), step(2));
 
     %  If step size is smaller than tolerance, break
     if norm(step) < tol
         fprintf('Step size is smaller than the tolerance. Breaking on iter:%d...\n', i);
         break;
     end
-
+    fprintf("Norm is: %s\n", norm(step))
     %  Update x
     x = x - step;
     % fprintf("Updated x on iteration %d is: %s\n", i, x)
 
     % Print the current state
-    fprintf('After iteration %d, x = [%f, %f]\n', i, x(1), x(2));
+    fprintf('After iteration %d, x = [%.3f, %.3f]\n\n', i, x(1), x(2));
+    % fprintf('\n')
     
 end
 
 % Print the final state
-fprintf('After 10 iterations, x = [%f, %f]\n', x(1), x(2));
+fprintf('After 10 iterations, x = [%.3f, %.3f]\n', x(1), x(2));
